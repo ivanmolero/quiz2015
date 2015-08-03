bc7b6267dac925c8b7b023618c1859a1e3649822
ivanmolero <imolerod@gmail.com>
2015-08-03 15:52:06 -0500

    validacion de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,16 +2,25 @@ var models = require('../models/models.js');
 
 exports.create = function(req, res){
   var quiz = models.Quiz.build(req.body.quiz);
-  quiz.save({fields:["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })
+  quiz.validate().then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors})
+      } else {
+        quiz.save({fields:["pregunta", "respuesta"]}).then(function(){
+          res.redirect('/quizes');
+      })
+    }
+  }
+  );
+
 };
 
 exports.new = function(req, res) {
   var quiz = models.Quiz.build(
     {pregunta: 'Pregunta', respuesta: 'Respuesta'}
   );
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 exports.load = function(req, res, next, quizId) {
@@ -27,12 +36,12 @@ exports.load = function(req, res, next, quizId) {
 
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes){
-    res.render('quizes/index.ejs', {quizes: quizes});
+    res.render('quizes/index.ejs', {quizes: quizes, errors: []});
   }).catch(function(error) {next(error);})
 };
 
 exports.show = function(req, res){
-  res.render('quizes/show', {quiz: req.quiz});
+  res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 //exports.question = function(req, res) {
@@ -46,5 +55,5 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado})
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []})
 };
